58985f40f557716a83748b71eaa5f2035ae10559
totarochristian <totaro.christian.18@gmail.com>
2023-05-05 10:23:28 +0200

    Update the hotels description in the data array

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     $hotels = [
         [
             'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
+            'description' => 'Il 4 stelle Hotel Belvedere Riccione offre momenti di relax nell\'area spa per adulti & tanti servizi per bambini & famiglie',
             'parking' => true,
             'vote' => 4,
             'distance_to_center' => 10.4,
@@ -26,7 +26,7 @@
         ],
         [
             'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
+            'description' => 'L\'Hotel Futuro si trova nel centro di Aversa, a 5 minuti d\'auto dalla stazione, è facilmente raggiungibile dall\'autostrada A1 e propone camere climatizzate',
             'parking' => true,
             'vote' => 2,
             'distance_to_center' => 2,
@@ -34,7 +34,7 @@
         ],
         [
             'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
+            'description' => 'Goditi il sole e le spiagge del Lido di Venezia con l\'ospitalità di Hotel Rivamare. Prenota Hotel 3 stelle al Lido di Venezia, a 20 minuti da Venezia',
             'parking' => false,
             'vote' => 1,
             'distance_to_center' => 1,
@@ -42,7 +42,7 @@
         ],
         [
             'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
+            'description' => 'Hotel Bellavista si trova a Pinzolo a 800 metri dagli impianti di risalita. Bellavista è un family hotel con ricchi programmi di animazione per i bambini',
             'parking' => false,
             'vote' => 5,
             'distance_to_center' => 5.5,
@@ -50,7 +50,7 @@
         ],
         [
             'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
+            'description' => 'Ospitalità di Montagna in Val Seriana nel nostro Hotel con SPA, Centro Benessere e Ristorante per gustare le nostre delizie a tutte le ore del giorno',
             'parking' => true,
             'vote' => 2,
             'distance_to_center' => 50,
