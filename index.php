231ba75b6e591ac038264e9a129ba29ef86a2d58
totarochristian <totaro.christian.18@gmail.com>
2023-05-05 11:13:16 +0200

    Add h2 to tell the user no results founded

--- index.php
+++ index.php
@@ -104,9 +104,14 @@
   </nav>
 
   <!-- Hotel cards container -->
-  <div class="p-4 mt-4 mb-4 d-flex justify-content-center align-items-stretch gap-4 flex-wrap">
-    <?php foreach($hotels as $elem){?>
-      <?php if((isset($filterParking) && ($filterParking<0 || $filterParking==$elem["parking"]))&&($filterVote==0 || $filterVote<=$elem['vote'])&&(str_contains(strtolower($elem['name']),strtolower($filterName)))){ ?>
+  <div id="hotelsContainer" class="p-4 mt-4 mb-4 d-flex justify-content-center align-items-stretch gap-4 flex-wrap">
+    <?php
+      $counter = 0;
+      foreach($hotels as $elem){?>
+      <?php 
+        if((isset($filterParking) && ($filterParking<0 || $filterParking==$elem["parking"]))&&($filterVote==0 || $filterVote<=$elem['vote'])&&(str_contains(strtolower($elem['name']),strtolower($filterName)))){ 
+          $counter += 1;
+        ?>
         <div class="card" style="width: 18rem;">
           <img src="./assets/images/<?php echo $elem['image'] ?>" class="card-img-top" alt="...">
           <div class="card-body">
@@ -127,7 +132,10 @@
           </div>
         </div>
       <?php } ?>
-    <?php } ?>
+    <?php } 
+      if($counter == 0)
+        echo '<h2>Non ci sono risultati per la tua ricerca!</h2>';
+    ?>
   </div>
 </body>
 </html>
