86b049d42b51446822c533a8311c66f7cd53b41e
totarochristian <totaro.christian.18@gmail.com>
2023-05-05 10:18:33 +0200

    Refaactoring of the code

--- index.php
+++ index.php
@@ -63,42 +63,44 @@
     $filterVote = isset($_GET['vote']) && !empty($_GET['vote']) ? $_GET['vote'] : 0;
   ?>
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#" class="d-flex align-items-center">
-      <img src="./assets/images/icon.png" alt="Bootstrap" width="55" height="60">
-      <span>Hotels</span>
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <form action="<?php echo $_SERVER['PHP_SELF']?>" method="GET" class="w-100 p-3 d-flex justify-content-center align-items-center gap-4 flex-wrap">
-        <!-- Name filter -->
-        <div class="d-flex gap-2 align-items-center">
-          <label for="vote">Nome: </label>
-          <input type="text" name="name" id="name" maxlength="20" value="<?php echo $filterName ?>">
-        </div>
-        <!-- Parking filter -->
-        <div class="d-flex gap-2 align-items-center">
-          <label for="parking">Parcheggio: </label>
-          <select name="parking" id="parking">
-            <option value="-1" <?php if($filterParking == -1) echo 'selected' ?>>Tutti i risultati</option>
-            <option value="1" <?php if($filterParking == 1) echo 'selected' ?>>Incluso</option>
-            <option value="0" <?php if($filterParking == 0) echo 'selected' ?>>Non incluso</option>
-          </select>
-        </div>
-        <!-- Vote filter -->
-        <div class="d-flex gap-2 align-items-center">
-          <label for="vote">Voto: </label>
-          <input type="number" name="vote" id="vote" max="5" min="0" value="<?php echo $filterVote ?>">
-        </div>
-        <!-- Submit btn -->
-        <input type="submit" value="Cerca">
-      </form>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#" class="d-flex align-items-center">
+        <img src="./assets/images/icon.png" alt="Bootstrap" width="55" height="60">
+        <span>Hotels</span>
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <!-- Form with the filters -->
+        <form action="<?php echo $_SERVER['PHP_SELF']?>" method="GET" class="w-100 p-3 d-flex justify-content-center align-items-center gap-4 flex-wrap">
+          <!-- Name filter -->
+          <div class="d-flex gap-2 align-items-center">
+            <label for="vote">Nome: </label>
+            <input type="text" name="name" id="name" maxlength="20" value="<?php echo $filterName ?>">
+          </div>
+          <!-- Parking filter -->
+          <div class="d-flex gap-2 align-items-center">
+            <label for="parking">Parcheggio: </label>
+            <select name="parking" id="parking">
+              <option value="-1" <?php if($filterParking == -1) echo 'selected' ?>>Tutti i risultati</option>
+              <option value="1" <?php if($filterParking == 1) echo 'selected' ?>>Incluso</option>
+              <option value="0" <?php if($filterParking == 0) echo 'selected' ?>>Non incluso</option>
+            </select>
+          </div>
+          <!-- Vote filter -->
+          <div class="d-flex gap-2 align-items-center">
+            <label for="vote">Voto: </label>
+            <input type="number" name="vote" id="vote" max="5" min="0" value="<?php echo $filterVote ?>">
+          </div>
+          <!-- Submit btn -->
+          <input type="submit" value="Cerca">
+        </form>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
 
   <!-- Hotel cards container -->
   <div class="p-4 mt-4 mb-4 d-flex justify-content-center align-items-stretch gap-4 flex-wrap">
@@ -107,13 +109,16 @@
         <div class="card" style="width: 18rem;">
           <img src="./assets/images/<?php echo $elem['image'] ?>" class="card-img-top" alt="...">
           <div class="card-body">
+            <!-- Vote defined printing stars -->
             <p class="card-text">
               <?php for($i=0; $i<$elem["vote"]; $i++){?>
                 <i class="fa-solid fa-star"></i>
               <?php } ?>
             </p>
+            <!-- Title and description of the hotel -->
             <h5 class="card-title"><?php echo $elem["name"] ?></h5>
             <p class="card-text"><?php echo $elem["description"] ?></p>
+            <!-- Badges in the bottom of the card -->
             <p class="card-text badge rounded-pill text-bg-primary"><?php echo $elem["distance_to_center"] ?> km dal centro</p>
             <?php if($elem["parking"]){ ?>
               <p class="card-text badge rounded-pill text-bg-primary">parcheggio</p>
