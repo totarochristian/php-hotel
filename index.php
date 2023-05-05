3182276be299fcf1aa6f0df6ef71eebcb297e0aa
totarochristian <totaro.christian.18@gmail.com>
2023-05-05 09:30:57 +0200

    Add basic styles

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
   <!-- Import bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-  <!-- Import font-awesome -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- Include fontawesome library -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
   <?php
@@ -63,14 +63,17 @@
     <div class="card" style="width: 18rem;">
       <img src="./assets/images/<?php echo $elem['image'] ?>" class="card-img-top" alt="...">
       <div class="card-body">
+        <p class="card-text">
+          <?php for($i=0; $i<$elem["vote"]; $i++){?>
+            <i class="fa-solid fa-star"></i>
+          <?php } ?>
+        </p>
         <h5 class="card-title"><?php echo $elem["name"] ?></h5>
         <p class="card-text"><?php echo $elem["description"] ?></p>
-        <p class="card-text"><strong>Distanza dal centro:</strong> <?php echo $elem["distance_to_center"] ?> km</p>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item"><?php echo $elem["parking"] ?></li>
-          <li class="list-group-item"><?php echo $elem["vote"] ?></li>
-          <li class="list-group-item"><?php echo $elem["distance_to_center"] ?></li>
-        </ul>
+        <p class="card-text badge rounded-pill text-bg-primary"><?php echo $elem["distance_to_center"] ?> km dal centro</p>
+        <?php if($elem["parking"]){ ?>
+          <p class="card-text badge rounded-pill text-bg-primary">parcheggio</p>
+        <?php } ?>
       </div>
     </div>
     <?php } ?>
