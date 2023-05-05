809ed143de2083caa23ec85cb68165e0d3bf773e
totarochristian <totaro.christian.18@gmail.com>
2023-05-05 09:16:38 +0200

    Add hotel images in assets folder and in the array

--- index.php
+++ index.php
@@ -19,35 +19,40 @@
             'description' => 'Hotel Belvedere Descrizione',
             'parking' => true,
             'vote' => 4,
-            'distance_to_center' => 10.4
+            'distance_to_center' => 10.4,
+            'image' => 'hotel-belvedere.jpg'
         ],
         [
             'name' => 'Hotel Futuro',
             'description' => 'Hotel Futuro Descrizione',
             'parking' => true,
             'vote' => 2,
-            'distance_to_center' => 2
+            'distance_to_center' => 2,
+            'image' => 'hotel-futuro.jpeg'
         ],
         [
             'name' => 'Hotel Rivamare',
             'description' => 'Hotel Rivamare Descrizione',
             'parking' => false,
             'vote' => 1,
-            'distance_to_center' => 1
+            'distance_to_center' => 1,
+            'image' => 'hotel-rivamare.jpg'
         ],
         [
             'name' => 'Hotel Bellavista',
             'description' => 'Hotel Bellavista Descrizione',
             'parking' => false,
             'vote' => 5,
-            'distance_to_center' => 5.5
+            'distance_to_center' => 5.5,
+            'image' => 'hotel-bellavista.jpg'
         ],
         [
             'name' => 'Hotel Milano',
             'description' => 'Hotel Milano Descrizione',
             'parking' => true,
             'vote' => 2,
-            'distance_to_center' => 50
+            'distance_to_center' => 50,
+            'image' => 'hotel-milano.jpg'
         ]
     ];
   ?>
