05d83b265d13139f51a37619bd333850d8b3d9eb
totarochristian <totaro.christian.18@gmail.com>
2023-05-05 12:01:05 +0200

    Remove php closing tag because the file haven't html code

--- partials/hotelData.php
+++ partials/hotelData.php
@@ -45,4 +45,3 @@
     $filterName = isset($_GET['name']) ? $_GET['name'] : "";
     $filterParking = isset($_GET['parking']) ? $_GET['parking'] : -1;
     $filterVote = isset($_GET['vote']) && !empty($_GET['vote']) ? $_GET['vote'] : 1;
-?>
